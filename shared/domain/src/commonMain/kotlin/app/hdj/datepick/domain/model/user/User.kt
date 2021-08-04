f6d0dad97e9e9c9b3047d5ba3eacd43f6f68c566
Harry Park <jungsanmango@gmail.com>
2021-08-04 17:29:09 +0900

    [feature/DATEPICK-60]: User isMe Property 구현부 수정

--- shared/domain/src/commonMain/kotlin/app/hdj/datepick/domain/model/user/User.kt
+++ shared/domain/src/commonMain/kotlin/app/hdj/datepick/domain/model/user/User.kt
@@ -3,6 +3,6 @@ package app.hdj.datepick.domain.model.user
 interface User {
     val id: String
     val nickname: String
-    val isMe : Boolean get() = false
+    val isMe : Boolean
     val profileImageUrl: String?
 }
