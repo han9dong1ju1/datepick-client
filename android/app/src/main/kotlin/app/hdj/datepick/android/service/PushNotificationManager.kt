a30fed43c77b5255aeb511087325b64fc74e3096
HarryPark <jungsanmango@gmail.com>
2021-09-19 17:30:03 +0900

    Push Notification 셋업

---
+++ android/app/src/main/kotlin/app/hdj/datepick/android/service/PushNotificationManager.kt
@@ -0,0 +1,63 @@
+package app.hdj.datepick.android.service
+
+import android.content.Context
+import android.graphics.Bitmap
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import androidx.core.content.getSystemService
+import androidx.core.graphics.drawable.toBitmap
+import app.hdj.datepick.domain.model.pushNotification.PushNotificationData
+import coil.imageLoader
+import coil.request.ImageRequest
+import dagger.hilt.android.qualifiers.ApplicationContext
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import javax.inject.Inject
+import kotlin.coroutines.CoroutineContext
+
+interface PushNotificationManager {
+
+    fun showNotification(pushNotificationData: PushNotificationData)
+
+}
+
+class PushNotificationManagerImp @Inject constructor(
+    @ApplicationContext private val context: Context
+) : PushNotificationManager, CoroutineScope {
+
+    override val coroutineContext: CoroutineContext
+        get() = Dispatchers.Main
+
+    private fun createNotificationChannels() {
+
+    }
+
+    init {
+        createNotificationChannels()
+    }
+
+    override fun showNotification(pushNotificationData: PushNotificationData) {
+
+        NotificationManagerCompat.from(context).notify(0, context.buildNotification())
+    }
+
+    private suspend fun String.loadIconBitmap(
+        requestBuilder: ImageRequest.Builder.() -> ImageRequest.Builder
+    ): Bitmap? {
+
+        val request = ImageRequest.Builder(context = context)
+            .data(this)
+            .dispatcher(Dispatchers.IO)
+            .requestBuilder()
+            .build()
+
+        return context.imageLoader.execute(request).drawable?.toBitmap()
+    }
+
+    private fun Context.buildNotification(
+
+    ) = NotificationCompat.Builder(this, "")
+            .setContentTitle("")
+            .build()
+
+}
--- android/app/src/main/kotlin/app/hdj/datepick/android/service/PushNotificationService.kt
+++ android/app/src/main/kotlin/app/hdj/datepick/android/service/PushNotificationService.kt
@@ -1,25 +1,46 @@
 package app.hdj.datepick.android.service
 
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import androidx.core.graphics.drawable.IconCompat
+import androidx.core.graphics.drawable.toBitmap
+import app.hdj.datepick.domain.model.pushNotification.PushNotificationData
+import coil.imageLoader
+import coil.request.ImageRequest
+import coil.transform.CircleCropTransformation
+import coil.transform.RoundedCornersTransformation
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.json.Json
+import javax.inject.Inject
 import kotlin.coroutines.CoroutineContext
 
 @AndroidEntryPoint
+@OptIn(ExperimentalSerializationApi::class)
 class PushNotificationService : FirebaseMessagingService(), CoroutineScope {
+
+    @Inject
+    lateinit var pushNotificationManager: PushNotificationManager
 
     override val coroutineContext: CoroutineContext = Dispatchers.IO
 
     override fun onMessageReceived(message: RemoteMessage) {
         super.onMessageReceived(message)
-
-
+        val params = message.data["params"] ?: return
+        val data = Json.decodeFromString<PushNotificationData>(params)
+        pushNotificationManager.showNotification(data)
     }
 
     override fun onNewToken(token: String) {
         super.onNewToken(token)
+
     }
 
 }
