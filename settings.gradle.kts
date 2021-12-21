02f8b9b182381f22b9ab8f7417aab24807078bff
Harry Park <jungsanmango@gmail.com>
2021-12-21 15:02:21 +0900

    [home-ui] 버전 업데이트

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,6 +10,8 @@ rootProject.name = "datepick-client"
 
 plugins {
     id("de.fayard.refreshVersions") version "0.23.0"
+////                            # available:"0.30.0"
+////                            # available:"0.30.1"
 }
 
 include(":android:app")
